3d17fd26e120d3dba5a99810c7a176a9884b7080
jessiejones <jessie_jones@iinet.net.au>
2020-10-16 08:58:51 +1100

    bb

---
+++ imagenets_web/assets/js/main.js
@@ -0,0 +1,163 @@
+$(document).ready(function(){
+	console.log("hello dpub");
+
+	$(".con").addClass("hide");
+
+	$(".why").addClass("hide");
+
+	$(".credits").addClass("hide");
+
+	$(".btn-skin").click(function(){
+		$(".con").addClass("hide");
+		$(".why").addClass("hide");
+		$(".item").removeClass("hide");
+		$(".con.skin").removeClass("hide");
+	});
+
+	// $(".btn-kids").click(function(){
+	// 	$(".con").addClass("hide");
+	// 	$(".con.kids").removeClass("hide");
+	// });
+
+	$(".btn-cook").click(function(){
+		$(".con").addClass("hide");
+		$(".why").addClass("hide");
+		$(".item").removeClass("hide");
+		$(".con.cook").removeClass("hide");
+	});
+
+	$(".btn-car").click(function(){
+		$(".con").addClass("hide");
+		$(".why").addClass("hide");
+		$(".item").removeClass("hide");
+		$(".con.car").removeClass("hide");
+	});
+
+	$(".btn-rest").click(function(){
+		$(".con").addClass("hide");
+		$(".why").addClass("hide");
+		$(".item").removeClass("hide");
+		$(".con.rest").removeClass("hide");
+	});
+
+	$(".btn-pure").click(function(){
+		$(".con").addClass("hide");
+		$(".why").addClass("hide");
+		$(".item").removeClass("hide");
+		$(".con.pure").removeClass("hide");
+	});
+
+	$(".btn-human").click(function(){
+		$(".con").addClass("hide");
+		$(".why").addClass("hide");
+		$(".item").removeClass("hide");
+		$(".con.human").removeClass("hide");
+	});
+
+	$(".btn-net").click(function(){
+		$(".con").addClass("hide");
+		$(".why").addClass("hide");
+		$(".item").removeClass("hide");
+		$(".con.net").removeClass("hide");
+	});
+
+	// $(".btn-mum").click(function(){
+	// 	$(".con").addClass("hide");
+	// 	$(".con.mum").removeClass("hide");
+	// });
+
+	$(".btn-naughty").click(function(){
+		$(".con").addClass("hide");
+		$(".why").addClass("hide");
+		$(".item").removeClass("hide");
+		$(".con.naughty").removeClass("hide");
+	});
+
+	// $(".btn-mon").click(function(){
+	// 	$(".con").addClass("hide");
+	// 	$(".con.mon").removeClass("hide");
+	// });
+
+	// $(".btn-green").click(function(){
+	// 	$(".con").addClass("hide");
+	// 	$(".con.green").removeClass("hide");
+	// });
+
+	// $(".btn-create").click(function(){
+	// 	$(".con").addClass("hide");
+	// 	$(".con.create").removeClass("hide");
+	// });
+
+	$(".btn-art").click(function(){
+		$(".con").addClass("hide");
+		$(".why").addClass("hide");
+		$(".item").removeClass("hide");
+		$(".con.art").removeClass("hide");
+	});
+
+	$(".btn-ppl").click(function(){
+		$(".con").addClass("hide");
+		$(".why").addClass("hide");
+		$(".item").removeClass("hide");
+		$(".con.ppl").removeClass("hide");
+	});
+
+	$(".btn-dark").click(function(){
+		$(".con").addClass("hide");
+		$(".why").addClass("hide");
+		$(".item").removeClass("hide");
+		$(".con.dark").removeClass("hide");
+	});
+
+	$(".btn-pro").click(function(){
+		$(".con").addClass("hide");
+		$(".why").addClass("hide");
+		$(".item").removeClass("hide");
+		$(".con.pro").removeClass("hide");
+	});
+
+	// $(".btn-inter").click(function(){
+	// 	$(".con").addClass("hide");
+	// 	$(".con.inter").removeClass("hide");
+	// });
+
+	$(".btn-ani").click(function(){
+		$(".con").addClass("hide");
+		$(".why").addClass("hide");
+		$(".item").removeClass("hide");
+		$(".con.ani").removeClass("hide");
+	});
+
+	$(".btn-chem").click(function(){
+		$(".con").addClass("hide");
+		$(".why").addClass("hide");
+		$(".item").removeClass("hide");
+		$(".con.chem").removeClass("hide");
+	});
+
+	$(".btn-reset").click(function(){
+		$(".con").addClass("hide");
+		$(".why").addClass("hide");
+		$(".item").removeClass("hide");
+	});
+
+	$(".btn-why").click(function(){
+		$(".con").addClass("hide");
+		$(".item").addClass("hide");
+		$(".why").removeClass("hide");
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
