9d01aa2cd7846db6f4e33f89849ad36c5ebe330d
jessiejones <jessie_jones@iinet.net.au>
2020-10-16 14:57:48 +1100

    yy

--- imagenets_web/assets/js/main.js
+++ imagenets_web/assets/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
 	$(".con").addClass("hide");
 
-	$(".why").addClass("hide");
+	$(".side-by-side").addClass("hide");
 
 
 	$(".btn-menu").click(function(){
@@ -13,138 +13,112 @@ $(document).ready(function(){
 
 	$(".btn-skin").click(function(){
 		$(".con").addClass("hide");
-		$(".why").addClass("hide");
-		// $(".credits").addClass("hide");
+		$(".side-by-side").addClass("hide");
 		$(".item").removeClass("hide");
 		$(".con.skin").removeClass("hide");
 	});
 
 	$(".btn-cook").click(function(){
 		$(".con").addClass("hide");
-		$(".why").addClass("hide");
+		$(".side-by-side").addClass("hide");
 		$(".item").removeClass("hide");
 		$(".con.cook").removeClass("hide");
 	});
 
 	$(".btn-car").click(function(){
 		$(".con").addClass("hide");
-		$(".why").addClass("hide");
+		$(".side-by-side").addClass("hide");
 		$(".item").removeClass("hide");
 		$(".con.car").removeClass("hide");
 	});
 
 	$(".btn-rest").click(function(){
 		$(".con").addClass("hide");
-		$(".why").addClass("hide");
+		$(".side-by-side").addClass("hide");
 		$(".item").removeClass("hide");
 		$(".con.rest").removeClass("hide");
 	});
 
 	$(".btn-pure").click(function(){
 		$(".con").addClass("hide");
-		$(".why").addClass("hide");
+		$(".side-by-side").addClass("hide");
 		$(".item").removeClass("hide");
 		$(".con.pure").removeClass("hide");
 	});
 
 	$(".btn-human").click(function(){
 		$(".con").addClass("hide");
-		$(".why").addClass("hide");
+		$(".side-by-side").addClass("hide");
 		$(".item").removeClass("hide");
 		$(".con.human").removeClass("hide");
 	});
 
 	$(".btn-net").click(function(){
 		$(".con").addClass("hide");
-		$(".why").addClass("hide");
+		$(".side-by-side").addClass("hide");
 		$(".item").removeClass("hide");
 		$(".con.net").removeClass("hide");
 	});
 
-	// $(".btn-mum").click(function(){
-	// 	$(".con").addClass("hide");
-	// 	$(".con.mum").removeClass("hide");
-	// });
-
 	$(".btn-naughty").click(function(){
 		$(".con").addClass("hide");
-		$(".why").addClass("hide");
+		$(".side-by-side").addClass("hide");
 		$(".item").removeClass("hide");
 		$(".con.naughty").removeClass("hide");
 	});
 
-	// $(".btn-mon").click(function(){
-	// 	$(".con").addClass("hide");
-	// 	$(".con.mon").removeClass("hide");
-	// });
-
-	// $(".btn-green").click(function(){
-	// 	$(".con").addClass("hide");
-	// 	$(".con.green").removeClass("hide");
-	// });
-
-	// $(".btn-create").click(function(){
-	// 	$(".con").addClass("hide");
-	// 	$(".con.create").removeClass("hide");
-	// });
-
 	$(".btn-art").click(function(){
 		$(".con").addClass("hide");
-		$(".why").addClass("hide");
+		$(".side-by-side").addClass("hide");
 		$(".item").removeClass("hide");
 		$(".con.art").removeClass("hide");
 	});
 
 	$(".btn-ppl").click(function(){
 		$(".con").addClass("hide");
-		$(".why").addClass("hide");
+		$(".side-by-side").addClass("hide");
 		$(".item").removeClass("hide");
 		$(".con.ppl").removeClass("hide");
 	});
 
 	$(".btn-dark").click(function(){
 		$(".con").addClass("hide");
-		$(".why").addClass("hide");
+		$(".side-by-side").addClass("hide");
 		$(".item").removeClass("hide");
 		$(".con.dark").removeClass("hide");
 	});
 
 	$(".btn-pro").click(function(){
 		$(".con").addClass("hide");
-		$(".why").addClass("hide");
+		$(".side-by-side").addClass("hide");
 		$(".item").removeClass("hide");
 		$(".con.pro").removeClass("hide");
 	});
 
-	// $(".btn-inter").click(function(){
-	// 	$(".con").addClass("hide");
-	// 	$(".con.inter").removeClass("hide");
-	// });
-
 	$(".btn-ani").click(function(){
 		$(".con").addClass("hide");
-		$(".why").addClass("hide");
+		$(".side-by-side").addClass("hide");
 		$(".item").removeClass("hide");
 		$(".con.ani").removeClass("hide");
 	});
 
 	$(".btn-chem").click(function(){
 		$(".con").addClass("hide");
-		$(".why").addClass("hide");
+		$(".side-by-side").addClass("hide");
 		$(".item").removeClass("hide");
 		$(".con.chem").removeClass("hide");
 	});
 
 	$(".btn-reset").click(function(){
 		$(".con").addClass("hide");
-		$(".why").addClass("hide");
+		$(".side-by-side").addClass("hide");
 		$(".item").removeClass("hide");
 	});
 
 	$(".btn-why").click(function(){
 		$(".con").addClass("hide");
 		$(".item").addClass("hide");
-		$(".why").removeClass("hide");
+		$(".side-by-side").removeClass("hide");
 	});
 
 });
