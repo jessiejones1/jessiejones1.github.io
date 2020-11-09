983d0f9f83f2acd65e63c66d41733c7effa7c32b
jessiejones <jessie_jones@iinet.net.au>
2020-11-10 10:39:01 +1100

    n

--- not_a_book/assets/js/main.js
+++ not_a_book/assets/js/main.js
@@ -1,9 +1,3 @@
 $(document).ready(function () {
   console.log("im ready");
 
-  $(".button").click(function () {
-    console.log("you have clicked the button");
-
-    $(".menu").slideToggle();
-  });
-});
